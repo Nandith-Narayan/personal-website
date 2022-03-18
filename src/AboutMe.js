f30e6f2ad6fd964004e89dc89bb75f727c18f160
Nandith-Narayan <nandith2ndemail@gmail.com>
2022-03-18 19:13:47 -0400

    update

---
+++ src/AboutMe.js
@@ -0,0 +1,17 @@
+import "./AboutMe.css";
+
+function AboutMe(){
+    return (
+        <div className="card-container">
+            <div className="card">
+                <p>
+                    I'm an aspiring programmer and graduating senior at Florida Tech.
+                    My interests include formal methods, website design, emulation, machine learning, and competitive programming.
+                    Currently, I work as an undergraduate research assistant as part of the A.S.S.I.S.T. lab at Florida Tech.
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default AboutMe;
--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Navigation from "./Navigation";
 import {useState} from "react";
+import AboutMe from "./AboutMe";
 
 function App() {
     const [view, setView] = useState("about-me")
 
     return (
     <div className="App">
-      <Navigation view={view} setView={setView}/>
+        <Navigation view={view} setView={setView}/>
+        <AboutMe></AboutMe>
     </div>
     );
 }
