55f8fa1ec93972c39ea6cd3db08e35aa49922710
Nandith-Narayan <nandith2ndemail@gmail.com>
2022-03-18 19:24:34 -0400

    update

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -2,14 +2,14 @@ import "./AboutMe.css";
 
 function AboutMe(){
     return (
-        <div className="card-container">
-            <div className="card">
-                <p>
-                    I'm an aspiring programmer and graduating senior at Florida Tech.
-                    My interests include formal methods, website design, emulation, machine learning, and competitive programming.
-                    Currently, I work as an undergraduate research assistant as part of the A.S.S.I.S.T. lab at Florida Tech.
-                </p>
-            </div>
+        <div className="profile">
+            <img id="profile-pic" src="nnarayan.png" alt="profile"/>
+            <p>
+                I'm an aspiring software developer and graduating senior at Florida Tech.
+                My interests include formal methods, website design, emulation, machine learning, and competitive programming.
+                Currently, I work as an undergraduate research assistant as part of the A.S.S.I.S.T. lab at Florida Tech.
+            </p>
+
         </div>
     );
 }
